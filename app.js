a93e9ffbd85046eff0051885ea7268ce7b05c77e
Laureano <laugx2002@hotmail.com>
2025-11-20 00:02:37 -0300

    domain generator maid

---
+++ app.js
@@ -0,0 +1,27 @@
+const pronoun = ["my", "your", "our", "this", "that"];
+const noun = ["cloud", "byte", "node", "data"];
+const adjective = ["bold", "brightes", "swiftar", "clever"];
+const domainExtensions = [".com", ".io", ".net", ".ch", ".ar", ".es"];
+
+function getRandomPart(array) {
+  const randomIndex = Math.floor(Math.random() * array.length);
+  return array[randomIndex];
+}
+
+function domainList() {
+  let domain = "";
+  const domainNames = [];
+  for (const p of pronoun) {
+    for (const n of noun) {
+      for (const a of adjective) {
+        for (const d of domainExtensions) {
+          domain = p + n + a + d;
+          domainNames.push(domain);
+        }
+      }
+    }
+  }
+  return domainNames.join(", ");
+}
+
+console.log("Generated Domain Names: " + domainList());
